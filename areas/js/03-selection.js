a617dcb429094bc8626ea3733188a6876f0f41cd
Angeline <ammeitzler@gmail.com>
2020-12-03 14:04:42 -0500

    removed vue from selection room

--- areas/js/03-selection.js
+++ areas/js/03-selection.js
@@ -12,7 +12,6 @@ function updateEmotion(msg) {
   if (!curEmotion || curEmotion.name !== msg.name) {
     curEmotion = msg;
     console.log('emotion has been updated to: ' + msg.name + ' (base: ' + msg.base + ', level: ' + msg.level +')');
-
     updateInterface();
   }
 }
@@ -23,47 +22,28 @@ function updateInterface() {
 }
 
 
-// VUE
-const apiURL_emotions = "/emotions";
-var app;
-app = new Vue({
-  el: "#app",
-  data: {
-    emotions: null,
-    selection_txt: null,
-  },
-  created: function() {
-    this.fetchEmotions();
-    // this.fetchSelectionTxt();
-  },
-  methods: {
-    fetchEmotions: function() {
-      fetch(apiURL_emotions)
-        .then(response => response.json())
-        .then(data => { this.emotions = data })
-    },
-    joinedmode: function() {
-      $("#wrapper_joined").fadeIn(1000);
-      $("#wrapper_separate").fadeOut(1000);
-    },
-    separatemode: function(emotionName) {
-      socket.emit('emotion:pick', emotionName);
-      $("#wrapper_joined").fadeOut(1000);
-      $("#wrapper_separate").fadeIn(1000);
-    }
-  }
-});
-
 
 // VARIABLES
 const seperate_panel1 = document.getElementById("scroll1");
 const seperate_panel2 = document.getElementById("scroll2");
 const seperate_panel3 = document.getElementById("scroll3");
 const seperate_panel4 = document.getElementById("scroll4");
-const sel_txt_url = 'data/03_selection_intro.txt';
+const sel_txt_url = '/data/03_selection_intro.txt';
+const apiURL_emotions = "/emotions";
 
 let sel_intro_content;
+let emotions;
 let active = false;
+
+function joinedmode() {
+  $("#wrapper_joined").fadeIn(1000);
+  $("#wrapper_separate").fadeOut(1000);
+}
+function seperatemode(emotionName) {
+  socket.emit('emotion:pick', emotionName);
+  $("#wrapper_joined").fadeOut(1000);
+  $("#wrapper_separate").fadeIn(1000);
+}
 
 //timer for if the panels are not being used, go back to joinedmode
 //timer does not start until touched
@@ -72,7 +52,7 @@ function resetInterval() {
   var timer = setTimeout(function() {
     console.log("restarted interval");
     active = false
-    app.joinedmode()
+    joinedmode()
    }, 7000); 
 }
 seperate_panel1.onscroll = function (e) {
@@ -105,21 +85,36 @@ seperate_panel4.onscroll = function (e) {
 }
 
 
+// READ IN EMOTION JSON
+fetch(apiURL_emotions)
+  .then(response => response.json())
+  .then(data => { 
+    emotions = data;
+    for (let emotion in emotions) {
+        let $emotion_div = $("<div>", {
+          "class": "emotion", 
+          text: `${emotion}`,
+          "click": function() {
+            seperatemode(`${emotion}`)
+          }
+        });
+        $("#scroll_joined").append($emotion_div)
+    }
+  })
 
-
+// READ IN SELECTION TEXT
 fetch(sel_txt_url)
   .then(response => response.text())
   .then(text => sel_intro_content = text)
   .then(() => selection_txt_parse(sel_intro_content))
 
-
+// PARSING SELECTION TEXT TO PANELS
 const num_sents_panels = 3;
 function selection_txt_parse(sel_intro_content) {
   let sel_intro_sent = sel_intro_content.match( /[^\.!\?]+[\.!\?]+/g );
   const panel_array = new Array(Math.ceil(sel_intro_sent.length / num_sents_panels))
     .fill()
     .map(_ => sel_intro_sent.splice(0, num_sents_panels))
-  console.log(panel_array)
   for(var i = 0; i < panel_array.length; i++) {
       var sentences = panel_array[i];
       for(var j = 0; j < sentences.length; j++) {
