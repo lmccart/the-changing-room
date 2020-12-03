ffcffdf54c9d21692c02255fabfb494f9037fe38
samepant <sampanter@gmail.com>
2020-12-03 18:10:44 -0500

    retrieves and parses tsv data

--- areas/js/05-convo2.js
+++ areas/js/05-convo2.js
@@ -1,4 +1,6 @@
 // style and js imports
+import Papa from 'papaparse';
+
 import '../css/05-convo2.scss';
 import './shared.js';
 
@@ -19,3 +21,23 @@ function updateEmotion(msg) {
 function updateInterface() {
   $('#debug-info').text('CURRENT EMOTION: ' + curEmotion.name + ' (base: ' + curEmotion.base + ', level: ' + curEmotion.level +')')
 }
+
+Papa.parse("/data/05_directions.tsv", {
+  download: true,
+  header: true,
+  skipEmptyLines: 'greedy',
+  complete: function(results) {
+    const rawResults = results.data;
+    // the data comes in as [{angry: 'string', sad: 'string'}, ...]
+    // I (sam) think it should be {angry: ['string', 'string'], sad:['string', 'string']...}
+    const reordered = {};
+    const keys = Object.keys(rawResults[0]);
+    keys.forEach(key => reordered[key] = []);
+
+    for (var i = 0; i < rawResults.length; i++) {
+      const resultRow = rawResults[i];
+      keys.forEach(key => resultRow[key].trim().length > 0 && reordered[key].push(resultRow[key]));
+    }
+    window.instructions = reordered;
+  }
+});
