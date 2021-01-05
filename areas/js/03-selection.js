1032ef678cde537f8c91c2aca14587af253cf672
Lauren McCarthy <laurenleemccarthy@gmail.com>
2021-01-04 17:04:45 -0800

    clean up initial loading view

--- areas/js/03-selection.js
+++ areas/js/03-selection.js
@@ -7,6 +7,7 @@ import './shared.js';
 let curEmotion;
 const socket = io();
 socket.on('emotion:update', updateEmotion);
+$('#wrapper_separate').hide();
 
 function updateEmotion(msg) {
   console.log(msg)
