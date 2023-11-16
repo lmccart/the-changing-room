18558fc8cb3c61f2948188a28576cfad72bf6161
Lauren Lee McCarthy <laurenleemccarthy@gmail.com>
2023-11-15 18:06:42 -0800

    ok

---
+++ areas/js/07-rotating.js
@@ -0,0 +1,31 @@
+import $ from 'jquery';
+import '../css/07-rotating.scss';
+import './shared.js';
+
+setMode('reflection');
+
+window.init = () => {
+  console.log('init');
+  
+  socket.on('emotion:update', (msg) => {
+    console.log('EMOTION:UPDATE TOGGLE MODE');
+    toggleMode();
+  });
+};
+
+function toggleMode() {
+  let mode = window.soundType === 'reflection' ? 'environment' : 'reflection';
+  setMode(mode);
+}
+
+function setMode(m) {
+  console.log('set mode ', m);
+  window.soundType = m;
+  if (window.soundType === 'reflection') {
+    $('#passive-section').hide();
+    $('#reflection-section').show();
+  } else {
+    $('#passive-section').show();
+    $('#reflection-section').hide();
+  }
+}
